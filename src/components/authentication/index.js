736e68f74c0145452c580a5a7e96d9dad18cac8e
m-triple-m <triplem656@gmail.com>
2021-06-24 11:03:53 +0530

    ..

--- src/components/authentication/index.js
+++ src/components/authentication/index.js
@@ -2,7 +2,7 @@ import { Route, Switch, useRouteMatch } from "react-router";
 import Checkout from "../checkout";
 import Header from "../header";
 import ListCourses from "../listCourses";
-import Home from "./home";
+import Home from "../home";
 import Login from "./login";
 import Register from "./register";
 
@@ -20,7 +20,6 @@ const Main = props => {
                     <Route path={`${path}/register`} component={Register} />
                     <Route path={`${path}/list`} component={ListCourses} />
                     <Route path={`${path}/checkout`} component={Checkout} />
-                    <Route path={`${path}/home`} component={Home} />
                 </Switch>
 
             </div>
